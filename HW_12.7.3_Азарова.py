9c3eee43ecba05509d9cc9b9f57b38eecc4fd520
megaamfibia <119670976+megaamfibia@users.noreply.github.com>
2023-01-11 15:13:41 +0300

    Delete HW_12.7.3_Азарова.py

--- HW_12.7.3_Азарова.py
+++
@@ -1,7 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-list_per_cent = list(per_cent.values())
-money = int(input('Введите сумму: '))
-deposit=[]
-for i in list_per_cent:
-    deposit.append(int((i * money)/100))
-print('Максимальная сумма, которую вы можете заработать: ' + str(max(deposit)))
