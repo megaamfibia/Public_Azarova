8693e985b0fab1e5ddfc896b7b34a29200c25786
megaamfibia <119670976+megaamfibia@users.noreply.github.com>
2023-02-28 19:14:51 +0300

    Add files via upload

---
+++ HW_18.6_Azarova/extensions.py
@@ -0,0 +1,36 @@
+import json
+import requests
+from config import keys
+
+
+class ConvertionException(Exception):
+    pass
+
+
+class MyConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'невозможно перевести одинаковые валюты "{base}".')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'некорректно указана валюта "{quote}".\nЧтобы посмотреть список доступных валют,'
+                                      f' введите команду /values')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'некорректно указана валюта "{base}".\nЧтобы посмотреть список доступных валют, '
+                                      f'введите команду /values')
+
+        try:
+            amount = abs(float(amount))  # игнорируем минус, который ввел пользователь
+        except ValueError:
+            raise ConvertionException(f'некорректно указано количество "{amount}".')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base * amount
